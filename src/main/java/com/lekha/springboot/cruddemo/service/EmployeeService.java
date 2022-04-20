b29d790ac1c0ee6e7fec7c5f19001f8c8b19b5d6
lekha <lekharamadasa@gmail.com>
2022-04-20 18:36:09 -0300

    first commit

---
+++ src/main/java/com/lekha/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.lekha.springboot.cruddemo.service;
+
+
+import java.util.List;
+
+import com.lekha.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+}
